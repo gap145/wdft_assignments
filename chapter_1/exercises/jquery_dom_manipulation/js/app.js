092252b160e0d3d25d225db230d77ba26f28a65f
Gap145 <gregpaduano@gmail.com>
2018-01-28 19:27:26 -0500

    Project Committ

--- chapter_1/exercises/jquery_dom_manipulation/js/app.js
+++ chapter_1/exercises/jquery_dom_manipulation/js/app.js
@@ -1,1 +1,30 @@
+
+$(document).ready(function(){
+
+    $('#edit-header').text('The jQuery Challenge');
+   
+    $('#para1').css('font-size', '30px');
+    $('#para1').css('font-weight', 'bold');
+
+    $('.box1').css({"background-color": 'green'});
+   
+    $('.box2').addClass('spin');
+
+    $('#button1').click(function(){
+        alert('You clicked my button')
+    })
+
+    
+    $('.box3').hover(function(){
+            $(this).css("background-color", 'green');
+        });
+
+
+        $('.box4').animate({left:"80%"}, "slow");
+        $('.box4').animate({top:"60%"}, "slow");
+    })
+        
+      
+
+
 //Write your jQuery code in this file
---
+++ chapter_1/project/fan_website/albums.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#codebreak').click(function(){
+        window.location = 'albums/codebreak.html';
+    });    
+
+    $('#steps').click(function(){
+        window.location = 'albums/steps.html';
+    });    
+
+    $('#starwars').click(function(){
+        window.location = 'albums/starwars.html';
+    });    
+
+    $('#laundry').click(function(){
+        window.location = 'albums/laundry.html';
+    });    
+
+    $('#orange').click(function(){
+        window.location = 'albums/orange.html';
+    });    
+
+    $('#shoe').click(function(){
+        window.location = 'albums/shoe.html';
+    });    
+
+});
