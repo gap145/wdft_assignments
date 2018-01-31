554f98a7a1acda48b9efb8fc83719504a77e0eca
Gap145 <gregpaduano@gmail.com>
2018-01-30 22:11:01 -0500

    updated commit

--- chapter_2/exercises/loops/number_pyramid.js
+++ chapter_2/exercises/loops/number_pyramid.js
@@ -1,10 +1,21 @@
-// Int from 1-4 like pyramid, 1 having 1, 2, 2...
+// set it up as 1-4 like pyramid, 1 having 1, 2, 2...
 //    1
 //   2 2
 //  3 3 3
 // 4 4 4 4
-var pyramidArray = ['   1   ', '  2 2  ', ' 3 3 3 ', '4 4 4 4']
 
-for (i = 0; i < pyramidArray.length; i++) {
-    console.log(pyramidArray[i]);  // A lot easier to just deal with strings
-}
+//n is the number of lines in your pyramid  
+function printPyramid(value) {
+    if (value > 9 || value <= 0) {
+        console.log('Please pick a positive single digit number');
+    } else {
+        for (i = 1; i <= value; i++) {
+            var rowString = " ".repeat(value - i)  // Initial spacing
+            var midPortion = (String(i) + " ").repeat(i);  // Spaces and numbers repeated
+            rowString += midPortion;  // Combine both
+            console.log(rowString);
+        };
+    };
+};
+
+printPyramid(5);
