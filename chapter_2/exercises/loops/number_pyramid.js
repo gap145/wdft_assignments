913a740941cec36321b26b00607c4bcd4675898d
Gap145 <gregpaduano@gmail.com>
2018-01-30 22:00:23 -0500

    commit

---
+++ chapter_2/exercises/loops/number_pyramid.js
@@ -0,0 +1,10 @@
+// Int from 1-4 like pyramid, 1 having 1, 2, 2...
+//    1
+//   2 2
+//  3 3 3
+// 4 4 4 4
+var pyramidArray = ['   1   ', '  2 2  ', ' 3 3 3 ', '4 4 4 4']
+
+for (i = 0; i < pyramidArray.length; i++) {
+    console.log(pyramidArray[i]);  // A lot easier to just deal with strings
+}
---
+++ chapter_2/exercises/loops/number_triangle.js
@@ -0,0 +1,9 @@
+// Int from 1-4 like triangle, 1 having 1, 2, 2...
+// 1
+// 22
+// 333
+// 4444
+
+for (i = 1; i < 5; i++) {
+    console.log(String(i).repeat(i));  // Make number string and repeat string char x times
+}
